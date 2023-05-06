19d22b85000dee09283bd3ab5a351651b8524d60
lukieoo <pawkrzysciak@gmail.com>
2023-05-06 22:55:52 +0200

    Added Authorization to app.

---
+++ public/views/base/slider.php
@@ -0,0 +1,13 @@
+<div class="slide">
+    <a id="hamburger" href="#" onclick="openSlideMenu()">
+        <i class="fas fa-bars"></i>
+    </a>
+</div>
+<div id="menu" class="nav">
+    <a href="#" class="close" onclick="closeSlideMenu()">
+        <i class="fas fa-times"></i>
+    </a>
+    <a href="#" onclick="closeSlideMenu()">Home</a>
+    <a href="#" onclick="closeSlideMenu()">Find an Expert</a>
+    <a id="logIn" href="login" onclick="closeSlideMenu()">LogIn</a>
+</div>
---
+++ public/views/home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('base/head.php')
+?>
+<body>
+<?php
+include('base/slider.php')
+?>
+<div class="wrapper">
+    <div class="main">
+        <?php
+        include('base/nav.php')
+        ?>
+        <section class="hero">
+            <img src="public/img/main_image.jpeg" alt="Placeholder image">
+            <div class="hero-content">
+                <h1>Find the Best IT Experts - Your Center for Specialist Services</h1>
+                <p>Welcome to your center for IT specialist services! Our platform allows for easy and fast search
+                    for the best IT experts. From programmers and web developers to experts in cybersecurity and
+                    artificial intelligence, we offer a wide selection of specialists from different fields.</p>
+            </div>
+        </section>
+        <section class="features">
+            <div class="feature">
+                <h2>Rich Database of IT Specialists</h2>
+                <p>Find an expert that meets your requirements: Our platform offers a rich database of IT
+                    specialists with various skills and experience.</p>
+            </div>
+            <div class="feature">
+                <h2>Custom Projects</h2>
+                <p>Collaborate with IT experts to develop software tailored to your needs: Do you need software
+                    tailored to your needs? Our platform allows you to collaborate with IT experts that will develop
+                    software customized to your requirements.</p>
+            </div>
+            <div class="feature">
+                <h2>Verified Users</h2>
+                <p>Safer and more effective collaboration with IT experts: Our platform ensures the safety of our
+                    users by verifying the profiles of our IT specialists.</p>
+            </div>
+        </section>
+    </div>
+    <?php
+    include('base/footer.php')
+    ?>
+</div>
+</body>
+
+</html>
