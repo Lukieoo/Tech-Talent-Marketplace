edd21839c53f56a06c5d042c266bba6376144c04
lukieoo <pawkrzysciak@gmail.com>
2023-06-03 17:39:49 +0200

    Added edited and customise experts list

---
+++ src/controllers/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+class ProfileController extends AppController
+{
+    private $message = [];
+    private $expertsRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->expertsRepository = new ExpertsRepository();
+    }
+
+    public function edit()
+    {
+        session_start();
+
+        if (!$this->isPost()) {
+            $this->render('edit', ['expert' => $this->expertsRepository->getExpertFromID($_GET['id'] ?? $_SESSION['id']),
+                'isProfile' => (($_GET['id'] == null && $_SESSION['id'] != null) || ($_SESSION['id'] == $_GET['id'] && $_SESSION['id'] != null))
+            ]);
+            return;
+        }
+
+
+        $profession = $_POST['profession'];
+        $description = $_POST['description'];
+        $name = $_POST['name'];
+        echo $description;
+        $this->expertsRepository->updateExpert($_SESSION['id'], $profession, $description, $name);
+        $this->renderProfile();
+    }
+
+    public function profile()
+    {
+        session_start();
+        $this->renderProfile();
+
+    }
+
+    public function renderProfile(): void
+    {
+        $this->render('profile', ['expert' => $this->expertsRepository->getExpertFromID($_GET['id'] ?? $_SESSION['id']),
+            'isProfile' => (($_GET['id'] == null && $_SESSION['id'] != null) || ($_SESSION['id'] == $_GET['id'] && $_SESSION['id'] != null))
+        ]);
+    }
+}
